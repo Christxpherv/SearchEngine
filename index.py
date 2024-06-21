1dbb0b95451b97d26bc2cd9fd41941e022e9ac2d
Christopher Valle <christxpherv@gmail.com>
2024-06-21 15:29:46 -0500

    Defined the schema for document indexing

--- index.py
+++ index.py
@@ -2,3 +2,6 @@ import os
 import json
 from whoosh.index import create_in, open_dir
 from whoosh.fields import Schema, TEXT, ID
+
+# Define schema
+schema = Schema(title=TEXT(stored=True), url=ID(stored=True), content=TEXT)
