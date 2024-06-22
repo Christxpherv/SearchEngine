71552d60d879d9a690fc96645ba869fa322e911a
Christopher Valle <christxpherv@gmail.com>
2024-06-21 20:15:28 -0500

    Modified the crawler code so that it can extract the content of the urls

--- crawler.py
+++ crawler.py
@@ -5,6 +5,16 @@ class SimpleSpider(scrapy.Spider):
     start_urls = ['http://example.com']
 
     def parse(self, response):
+        # Extract the content from the <body> tag
+        content = ' '.join(response.css('body *::text').getall())
+        
+        # Yield a dictionary with 'title', 'url', and 'content' keys
+        yield {
+            'title': response.css('title::text').get(),
+            'url': response.url,
+            'content': content.strip()  # Store content as a string
+        }
+
         for href in response.css('a::attr(href)').getall():
+            # Call the parse method for each link
             yield response.follow(href, self.parse)
-        yield {'title': response.css('title::text').get(), 'url': response.url}
