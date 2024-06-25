edf52338d4d46028a5cae98d8b102f16934b714b
Christopher Valle <christxpherv@gmail.com>
2024-06-25 09:02:45 -0500

    modified the crawler so that it strips the whitespace in the json file and the \n

--- crawler.py
+++ crawler.py
@@ -5,16 +5,17 @@ class SimpleSpider(scrapy.Spider):
     start_urls = ['http://example.com']
 
     def parse(self, response):
-        # Extract the content from the <body> tag
+        # Extract the content from the <body> tag and clean it up
         content = ' '.join(response.css('body *::text').getall())
+        cleaned_content = ' '.join(content.split())  # Remove extra whitespace and newlines
         
         # Yield a dictionary with 'title', 'url', and 'content' keys
         yield {
             'title': response.css('title::text').get(),
             'url': response.url,
-            'content': content.strip()  # Store content as a string
+            'content': cleaned_content 
         }
 
+        # Follow all links on the page
         for href in response.css('a::attr(href)').getall():
-            # Call the parse method for each link
             yield response.follow(href, self.parse)
